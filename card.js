4160b510268dfa894f5488110cca35ed61c56e8e
Johnny Cassidy <johnny.k.cassidy@gmail.com>
2019-11-03 16:55:08 -0700

    Create class constructor

---
+++ card.js
@@ -0,0 +1,15 @@
+class Card {
+  constructor(matchInfo) {
+    // this.name = `card-${matchinfo}-${side}`
+    this.matchInfo = matchInfo;
+    this.id = Date.now();
+    this.matched = false;
+
+  }
+
+  // match(matchInfo) {
+  // if(matchInfo === matchInfo)
+  // console.log(matchInfo);
+  //
+  // }
+}
