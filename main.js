c7a2267d6b2a0a38beeca24cfa5f87846a0e6f84
Johnny Cassidy <johnny.k.cassidy@gmail.com>
2019-11-05 08:46:09 -0700

    Add flipping functionality

--- main.js
+++ main.js
@@ -3,12 +3,66 @@ var playerOneName = document.querySelector(".player-name");
 var playerTwoName = document.querySelector(".player-two");
 var page = document.querySelector(".intro-main");
 var main = document.querySelector("main");
-var card = document.querySelector(".card")
 var gameCards = [];
+var newDeck;
+var startTime;
+var endTime;
+
 
 startButton.addEventListener("click", formValidation);
-page.addEventListener("click", startGame)
-// main.addEventListener("click", flipCard)
+page.addEventListener("click", clickHandler)
+
+
+function clickHandler(event) {
+  if (event.target.classList.contains("overview-button")) {
+    startTime = Date.now();
+    startGame(event);
+  } if (event.target.classList.contains("card")){
+    flipCard(event);
+  }
+}
+
+function flipCard(event) {
+  var matchesThisRound = document.querySelector(".score-number")
+  // event.target.classList.contains("card")
+  if (newDeck.selectedCards.length >= 2 || event.target.classList.contains("back")) {
+    return;
+  } else if (event.target.classList.contains("card")) {
+    console.log(event)
+    console.log(newDeck)
+    console.log(event.target)
+    event.target.classList.add("flip");
+    var cardsDoMatch = newDeck.checkSelectedCards(event.target.dataset.id);
+    if (cardsDoMatch === true) {
+      matchesThisRound.innerHTML = newDeck.matches;
+      setTimeout(removeMatchedCards, 1000);
+      console.log(newDeck);
+    } if (cardsDoMatch === false && newDeck.selectedCards.length === 2) {
+      setTimeout(reflipCards, 1000)
+    }
+  }
+}
+
+function reflipCards() {
+  var cardOne = document.querySelectorAll(`[data-id='${newDeck.selectedCards[0].id}']`)
+  var cardTwo = document.querySelectorAll(`[data-id='${newDeck.selectedCards[1].id}']`)
+  for (var i = 0; i < 2; i++) {
+    cardOne[i].classList.remove("flip");
+    cardTwo[i].classList.remove("flip");
+    newDeck.selectedCards = [];
+  }
+}
+
+function removeMatchedCards() {
+  var cardOne = document.querySelectorAll(`[data-id='${newDeck.matchedCards[0].id}']`)
+  var cardTwo = document.querySelectorAll(`[data-id='${newDeck.matchedCards[1].id}']`)
+  for (var i = 0; i < 2; i++) {
+    cardOne[i].remove();
+    cardTwo[i].remove();
+    endTime = Date.now();
+    winPage();
+  }
+}
 
 function formValidation() {
   if (!playerOneName.value || playerOneName.value === "ENTER YOUR NAME") {
@@ -17,11 +71,13 @@ function formValidation() {
   } else goToInstructions(event);
 }
 
+
+
 function goToInstructions(event) {
   event.target.classList.contains("start-game")
   page.innerHTML = `<main class="overview-page">
     <section class="overview-content">
-      <h2 class="welcome">WELCOME ${playerOneName.value.toUpperCase()} NAME AND ${playerTwoName.value.toUpperCase()} NAME!</h2>
+      <h2 class="welcome">WELCOME ${playerOneName.value.toUpperCase()} AND ${playerTwoName.value.toUpperCase()}!</h2>
         <p class="description">The goal of the game is to find all 5 pairs of cards as quickly as possible. The player that finds the greatest number of pairs, wins</p>
         <p class="description">To begin playing, the player whose name is highlighted can click any card in the card pile. It will flip over and reveal a picture of Beyonc&eacute;. Click another card. If they match, they will disappear and you will have completed a match! If they don't, you'll have three seconds to look at them before they flip back over. Then it's time for the other player to try!</p>
         <p class="description">After you play, you'll see the name of the final winner and how long it took to win the game.</p>
@@ -31,88 +87,93 @@ function goToInstructions(event) {
   page.style.padding = "0px";
 }
 
+function timeConvert(time) {
+  var min = Math.floor(time / 60000);
+  var sec = ((time % 60000) / 1000).toFixed(0);
+  return (seconds == 60 ? (minutes+1) + ":00" : minutes + ":" + (seconds < 10 ? "0" : "") + seconds);
+}
+
+function winPage() {
+  if (newDeck.matchedCards.length === 10) {
+    main.innerHTML = `<main class="winner-page">
+      <h2 class="congrats">CONGRATULATIONS, ${playerOneName.value.toUpperCase()} WINS!</h2>
+      <span class="final-time">It took you ${timeConvert(endDate - startDate)} seconds.</span>
+      <span class="to-restart">Click below to keep playing.</span>
+      <div class="game-buttons">
+        <button class="new-game btn">NEW GAME</button>
+        <button class="rematch btn">REMATCH</button>
+      </div>
+    </main>`
+  } else return;
+}
+
 function createInstance() {
-  // debugger;
   for (var i = 0; i < 10; i++) {
-  var beyCard = new Card(i);
+  var beyCard = new Card(Math.floor(i/2), i);
   gameCards.push(beyCard);
 }
-  console.log(gameCards)
-  // gameCards.beyCard[i].matchInfo = i;
-  console.log(gameCards);
+  pushToDeck(gameCards)
   return gameCards;
 }
 
-
-function startGame (event) {
-  if (event.target.classList.contains("overview-button")) {
-    var newCard = createInstance();
-
-    page.innerHTML = `<main class="game-page">
-      <aside class="left">
-        <header class="game-header one">
-          <h3>PLAYER 1 NAME</h3>
-          <span class="top-player aside-text">#4 TOP PLAYER</span>
-        </header>
-        <section class="score one">
-          <span class="aside-text">MATCHES THIS ROUND</span>
-          <span class="score-number">5</span>
-        </section>
-        <footer>
-          <h3>GAME WINS</h3>
-          <div class="round-info"><span class="aside-text">ROUND 1</span><span class="aside-text time">1000 MINUTES</span></div>
-          <div class="round-info"><span class="aside-text">ROUND 1</span><span class="aside-text time">1000 MINUTES</span></div>
-        </footer>
-      </aside>
-      <section class="cards">
-
-      <div class="card-${newCard[0].matchInfo}-front card">B</div>
-      <div class="card-${newCard[0].matchInfo}-back card"></div>
-      <div class="card-${newCard[1].matchInfo}-front card">B</div>
-      <div class="card-${newCard[1].matchInfo}-back card"></div>
-      <div class="card-${newCard[2].matchInfo}-front card">B</div>
-      <div class="card-${newCard[2].matchInfo}-back card"></div>
-      <div class="card-${newCard[3].matchInfo}-front card">B</div>
-      <div class="card-${newCard[3].matchInfo}-back card"></div>
-      <div class="card-${newCard[4].matchInfo}-front card">B</div>
-      <div class="card-${newCard[4].matchInfo}-back card"></div>
-      <div class="card-${newCard[5].matchInfo}-front card">B</div>
-      <div class="card-${newCard[5].matchInfo}-back card"></div>
-      <div class="card-${newCard[6].matchInfo}-front card">B</div>
-      <div class="card-${newCard[6].matchInfo}-back card"></div>
-      <div class="card-${newCard[7].matchInfo}-front card">B</div>
-      <div class="card-${newCard[7].matchInfo}-back card"></div>
-      <div class="card-${newCard[8].matchInfo}-front card">B</div>
-      <div class="card-${newCard[8].matchInfo}-back card"></div>
-      <div class="card-${newCard[9].matchInfo}-front card">B</div>
-      <div class="card-${newCard[9].matchInfo}-back card"></div>
-
-      </section>
-      <aside class="right">
-        <header class="game-header two">
-          <h3>PLAYER 2 NAME</h3>
-          <span class="top-player aside-text">#4 PLAYER</span>
-        </header>
-        <section class="score two">
-          <span class="aside-text">MATCHES THIS ROUND</span>
-          <span class="score-number">5</span>
-        </section>
-        <footer>
-          <h3>GAME WINS</h3>
-          <div class="round-info"><span class="aside-text">ROUND 1</span><span class="aside-text time">1000 MINUTES</span></div>
-          <div class="round-info"><span class="aside-text">ROUND 1</span><span class="aside-text time">1000 MINUTES</span></div>
-        </footer>
-      </aside>
-    </main>`
-
-  } console.log(gameCards);
+function pushToDeck(gameCards) {
+  newDeck = new Deck(gameCards);
 }
 
-
-
-
-// function flipCard(event) {
-//   if (event.target.classList.contains("card").matchInfo === event.target.classList.contains("card").matchInfo){
-//     console.log(event);
-//   }
-// }
+function startGame (event) {
+  var newCard = createInstance();
+  page.innerHTML = `<main class="game-page">
+    <aside class="left">
+      <header class="game-header one">
+        <h3>${playerOneName.value}</h3>
+        <span class="top-player aside-text">#4 TOP PLAYER</span>
+      </header>
+      <section class="score one">
+        <span class="aside-text">MATCHES THIS ROUND</span>
+        <span class="score-number">${newDeck.matches}</span>
+      </section>
+      <footer>
+        <h3>GAME WINS</h3>
+        <div class="round-info"><span class="aside-text">ROUND 1</span><span class="aside-text time">1000 MINUTES</span></div>
+        <div class="round-info"><span class="aside-text">ROUND 1</span><span class="aside-text time">1000 MINUTES</span></div>
+      </footer>
+    </aside>
+    <section class="cards">
+      <div class="card-${newCard[0].matchInfo}-front front card" data-id="0">B</div>
+      <div class="card-${newCard[0].matchInfo}-back back card" data-id="0"></div>
+      <div class="card-${newCard[1].matchInfo}-front front match-zero card" data-id="1">B</div>
+      <div class="card-${newCard[1].matchInfo}-back back match-zero card" data-id="1"></div>
+      <div class="card-${newCard[2].matchInfo}-front front card" data-id="2">B</div>
+      <div class="card-${newCard[2].matchInfo}-back back card" data-id="2"></div>
+      <div class="card-${newCard[3].matchInfo}-front front match-one card" data-id="3">B</div>
+      <div class="card-${newCard[3].matchInfo}-back back match-one card" data-id="3"></div>
+      <div class="card-${newCard[4].matchInfo}-front front card" data-id="4">B</div>
+      <div class="card-${newCard[4].matchInfo}-back back card" data-id="4"></div>
+      <div class="card-${newCard[5].matchInfo}-front front match-two card" data-id="5">B</div>
+      <div class="card-${newCard[5].matchInfo}-back back match-two card" data-id="5"></div>
+      <div class="card-${newCard[6].matchInfo}-front front card" data-id="6">B</div>
+      <div class="card-${newCard[6].matchInfo}-back back card" data-id="6"></div>
+      <div class="card-${newCard[7].matchInfo}-front front match-three card" data-id="7">B</div>
+      <div class="card-${newCard[7].matchInfo}-back back match-three card" data-id="7"></div>
+      <div class="card-${newCard[8].matchInfo}-front front card" data-id="8">B</div>
+      <div class="card-${newCard[8].matchInfo}-back back card" data-id="8"></div>
+      <div class="card-${newCard[9].matchInfo}-front front match-four card" data-id="9">B</div>
+      <div class="card-${newCard[9].matchInfo}-back back match-four card" data-id="9"></div>
+    </section>
+    <aside class="right">
+      <header class="game-header two">
+        <h3>${playerTwoName.value}</h3>
+        <span class="top-player aside-text"></span>
+      </header>
+      <section class="score two">
+        <span class="aside-text">MATCHES THIS ROUND</span>
+        <span class="score-number">5</span>
+      </section>
+      <footer>
+        <h3>GAME WINS</h3>
+        <div class="round-info"><span class="aside-text">ROUND 1</span><span class="aside-text time">1000 MINUTES</span></div>
+        <div class="round-info"><span class="aside-text">ROUND 1</span><span class="aside-text time">1000 MINUTES</span></div>
+      </footer>
+    </aside>
+  </main>`
+}
